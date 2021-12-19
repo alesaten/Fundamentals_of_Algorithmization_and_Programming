31af9402a0c159ecd06b47b7d3c9b4277194f348
alexander <82850660+alesaten@users.noreply.github.com>
2021-12-19 16:23:20 +0300

    Add files via upload

---
+++ Лабораторная работа 2/HelloAppKAA/Form1.cs
@@ -0,0 +1,17 @@
+namespace HelloAppKAA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "Hello World!";
+            this.Size = new Size(200, 150);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.BackColor = Color.Aquamarine;
+            this.ForeColor = Color.Red;
+            this.Width = 250;
+            this.Height = 250;
+        }
+    }
+}
